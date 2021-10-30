17ac37a61d9ec3aa4e9dae910b3f4f364dbbffba
aungpyaekyawtechlearner <aungpyaekyawtechlearner@gmail.com>
2021-10-30 21:48:47 +0630

    image not transfer

--- projectPython/webServer/webTcpRequest/server.py
+++ projectPython/webServer/webTcpRequest/server.py
@@ -5,6 +5,18 @@
 
 import socket
 
+# def renderTemplate(path):
+#     if path == '/':
+#         #template = 'template/index.html'
+#         fin = open('template/index.html').read()
+#     elif path == '/about':
+#         template = 'template/about.html'
+#     else:
+#         content = "error"
+
+    #fin = open(template).read()
+    #content = fin.read()
+    #fin.close()
 
 # Define socket host and port
 SERVER_HOST = '127.0.0.1'
@@ -22,18 +34,38 @@ while True:
     client_connection, client_address = server_socket.accept()
 
     # Get the client request
-    request = client_connection.recv(1024).decode()
-    print(request)
+    request = client_connection.recv(1024).decode().split(' ')
+    path = request[1]
+    #print(f' requested Full stack : {request}')
+    request = request[-1].split('\n')[-1].split('&')
+    parameter =[]
+    value =[]
+    for a in request:
+        a = a.split('=')
+        if len(a) == 2:
+            parameter.append(a[0])
+            value.append(a[1])
+    request=dict(zip(parameter,value))
+    if all (key in request for key in ("fname","lname")):
+        print(f"Requested : {request}")
 
-    # Get the content of htdocs/index.html
-    fin = open('template/index.html')
-    content = fin.read()
-    fin.close()
+    if path.endswith('/'):
+        path =path[:-1]
+    if path == '':
+        #template = 'template/index.html'
+        content = open('template/index.html').read()
+    elif path == '/about':
+        content = open('template/about.html').read()
+    # else:
+    #      content = 'file  not found :3'
+
+    # if path == '/test.jpg':
+    #     content = open('template/test.jpg').read().encode()
 
     # Send HTTP response
-    response = 'HTTP/1.0 200 OK\n\n'+content
+    response = f'HTTP/1.0 200 OK\n\n'+content
     client_connection.sendall(response.encode())
     client_connection.close()
-
+.
 # Close socket
 server_socket.close()
